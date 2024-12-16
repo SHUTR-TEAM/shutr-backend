c6313688c7003937aab6ae584efdf56aa2b0233b
Dinuka Induwara <bandarainduwara3@gmail.com>
2024-12-16 19:18:07 +0530

    added authentication

---
+++ auth/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import PhotographerSignUpView, PhotographerSignInView, PhotographerLogoutView
+
+urlpatterns = [
+    path('signup/', PhotographerSignUpView.as_view(), name='photographer-signup'),
+    path('signin/', PhotographerSignInView.as_view(), name='photographer-signin'),
+    path('logout/', PhotographerLogoutView.as_view(), name='photographer-logout'),
+]
--- shutr_backend/urls.py
+++ shutr_backend/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
+    path('auth/', include('auth.urls')),  # Include the app's URLs
 ]
