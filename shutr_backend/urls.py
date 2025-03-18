2796ed6ca8f37b1aae048e69b0b9a0443bc2c360
Sashan Fernando <samithasashan2002@gmail.com>
2025-03-18 08:08:34 +0530

    Update urls.py

--- shutr_backend/urls.py
+++ shutr_backend/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('api/', include('chat.urls')),
     path('search/', include('search.urls')),
     path('auth/', include('auth.urls')),
-    path('events/', include('events.urls')),
+    #path('events/', include('events.urls')),
 ]
 
 
